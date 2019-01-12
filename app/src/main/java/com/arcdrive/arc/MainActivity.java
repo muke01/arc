f600dab930837244283f1c0ffcf6220d51bdebd3
nukat <ajdinnukic16@yahoo.com>
2019-01-12 14:31:15 +0100

    v3

---
+++ app/src/main/java/com/arcdrive/arc/MainActivity.java
@@ -0,0 +1,53 @@
+package com.arcdrive.arc;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final long START_TIME_IN_MILLIS = 600000;
+
+    private TextView mTextViewCountDown;
+
+    private CountDownTimer mCountDownTimer;
+
+
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Intent intent = new Intent(this, LoginSignupActivity.class);
+
+        mTextViewCountDown = findViewById(R.id.text_view_countdown);
+
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+                int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+                if(seconds == 56 && minutes == 9) {
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onFinish() {
+            }
+        }.start();
+    }
+}
